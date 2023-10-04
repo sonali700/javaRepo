41af50ffc1706d7fb755f87612394f342a4f33f9
sonali700 <145955960+sonali700@users.noreply.github.com>
2023-10-04 17:44:21 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
 	     System.out.println("demo");
+	    
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
